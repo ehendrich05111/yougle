d48e06eb0b5defed90df1af0bbf7daa275259fc0
unknown <ehendrich05111@gmail.com>
2022-10-17 14:53:22 -0400

    get er going

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 // TODO: change API base in production
-export const API_BASE = "http://localhost:9000";
-//export const API_BASE = "https://yougle.herokuapp.com"
+//export const API_BASE = "http://localhost:9000";
+export const API_BASE = "https://yougle.herokuapp.com";
 
 export const fetcher = (path, token) =>
   fetch(new URL(path, API_BASE), {
