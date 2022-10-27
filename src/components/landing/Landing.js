af96b63885a959f7be5312d9a47a6849d37ba875
black-tul1p <divay7@gmail.com>
2022-10-26 21:17:30 -0400

    Add profile page

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -2,12 +2,14 @@ import React from "react";
 import Search from "./Search";
 import Services from "./Services";
 import Navbar from "../Navbar";
+import Profile from "../Profile";
 import SavedMessages from "./SavedMessages";
 
 export const MainPages = {
   Search: "search",
   Services: "services",
   Saved: "saved",
+  Profile: "profile",
 };
 
 export default function Landing(props) {
@@ -18,6 +20,8 @@ export default function Landing(props) {
     content = <Services />;
   } else if (props.page === MainPages.Saved) {
     content = <SavedMessages />;
+  } else if (props.page === MainPages.Profile) {
+    content = <Profile />;
   } else {
     content = <p>Invalid page.</p>;
   }
